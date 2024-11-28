826fb185302cd1c53c18f6af1b9485329175bf49
iyz <iy.usa@outlook.com>
2024-11-27 22:54:20 -0500

    add database connection pooling logic

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,6 +1,9 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+)
 
 func OpenDB(dsn string) (*sql.DB, error) {
 
@@ -14,6 +17,10 @@ func OpenDB(dsn string) (*sql.DB, error) {
 		return nil, err
 	}
 
+	// db.SetMaxOpenConns(10) - don't use, creates artifical bottleneck of 10 connections
+	db.SetMaxIdleConns(5) 		// keeps up to 5 idle connections at a time on standby
+	db.SetConnMaxLifetime(10 * time.Minute) 		// recycles connections every 10 minutes
+	
 	return db, nil
 }
 
